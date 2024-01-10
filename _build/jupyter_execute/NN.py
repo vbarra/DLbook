763f4eecdf7dfcb3b28c124639a37cb3ae95ae25
barra <vincent.barra@uca.fr>
2024-01-10 15:05:20 +0100

    changes

--- _build/jupyter_execute/NN.py
+++ _build/jupyter_execute/NN.py
@@ -388,12 +388,20 @@
 # séparable et si les exemples sont présentés de manière équitable
 # (c'est-à-dire que la procédure de choix des exemples n'en exclut aucun),
 # la procédure d'apprentissage par correction d'erreur converge vers un
-# perceptron linéaire à seuil qui sépare linéairement ${\cal E}_a$.\
+# perceptron linéaire à seuil qui sépare linéairement ${\cal E}_a$.
+
+# In[1]:
+
+
+from IPython.display import Video
+Video("videos/correction_erreur_linsep.mp4",embed =True,width=500)
+
+
 # L'inconvénient majeur de cet apprentissage est que si l'échantillon
 # présenté n'est pas linéairement séparable, l'algorithme ne convergera
 # pas et l'on aura aucun moyen de le savoir.
 
-# In[1]:
+# In[2]:
 
 
 from IPython.display import Video
